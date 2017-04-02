d7eb974a2aadfb7e5b65532050619b7f03de2ac3
KeiLazu <kennixlazuardi@gmail.com>
2017-04-02 19:44:20 +0700

    initial commit: i had a mindblow

---
+++ Assets/LetterbyLetter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LetterbyLetter : MonoBehaviour {
+
+    [SerializeField]
+    protected Text Yuki;
+
+    string message;
+
+    private void Start()
+    {
+        message = Yuki.text;
+        Yuki.text = "";
+        StartCoroutine(Daisuki());
+
+    }
+
+    IEnumerator Daisuki()
+    {
+        foreach (char letter in message.ToCharArray())
+        {
+            Yuki.text += letter;
+            yield return new WaitForSeconds(0.2f);
+            
+        }
+
+    }
+
+}
